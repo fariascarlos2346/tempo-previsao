041605876a377d396bed614c4a0a543e88e79cc1
Carlos <fariascarlos2346@gmail.com>
2025-03-14 22:53:09 -0300

    fazendo a logica de programação

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+const Key = "8b68ea2758673ff898d434cdbfcc60c8"
+
+function buscarCidade() {
+
+}
+
+
+function cliqueiNoBotao(cidade) {
+    const cidade = document.querySelector(".input-cidade").value
+    
+    buscarCidade(cidade)
+}
